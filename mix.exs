cd4deb479a1e87c0de225557de299d01429fc682
Milad Rastian <milad@sam-media.com>
2016-03-16 01:48:09 +0800

    Run the test against multiple elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExGuard.Mixfile do
   def project do
     [app: :ex_guard,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
