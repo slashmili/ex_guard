0fc3f2d1aa99b478148bd9c3f100cc8792b3dd39
Milad Rastian <eslashmili@gmail.com>
2016-03-20 01:39:48 +0800

    Clean up

--- mix.exs
+++ mix.exs
@@ -18,22 +18,10 @@ defmodule ExGuard.Mixfile do
      source_url: "https://github.com/slashmili/ex_guard",]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:fs, "~> 0.9"},
      {:ex_doc, "~> 0.11.4", only: :dev},
