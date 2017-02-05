e4d4b2808e9f1dce65ee7799c0d9e783e220a4be
andyl <andy@r210.com>
2017-02-05 10:26:38 -0800

    Remove Elixir 1.4 compile warnings

--- lib/ex_guard/notifier.ex
+++ lib/ex_guard/notifier.ex
@@ -23,7 +23,7 @@ defmodule ExGuard.Notifier do
   def notify(opts) do
     opts = opts
       |> Keyword.put(:content_image, content_image(opts[:status]))
-      |> Keyword.put(:icon, get_icon)
+      |> Keyword.put(:icon, get_icon())
 
     @notifiers
     |> Enum.filter(fn (n) -> apply(n, :available?, []) end)
@@ -33,9 +33,9 @@ defmodule ExGuard.Notifier do
 
   defp content_image(status) do
     case status do
-      :ok -> "#{base_dir}/priv/icons/Success.icns"
-      :error -> "#{base_dir}/priv/icons/Failed.icns"
-      :pending -> "#{base_dir}/priv/icons/Failed.icns"
+      :ok -> "#{base_dir()}/priv/icons/Success.icns"
+      :error -> "#{base_dir()}/priv/icons/Failed.icns"
+      :pending -> "#{base_dir()}/priv/icons/Failed.icns"
       _ -> ""
     end
   end
