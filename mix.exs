2701ec2b044842c9b7546d90beb3e9ab262e38da
Milad Rastian <milad@high-mobility.com>
2018-04-04 12:01:03 +0200

    bump the version

--- mix.exs
+++ mix.exs
@@ -1,21 +1,22 @@
 defmodule ExGuard.Mixfile do
   use Mix.Project
 
-  @version "1.3.0"
+  @version "1.3.1"
 
   def project do
-    [app: :ex_guard,
-     version: @version,
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     package: package(),
-     description: description(),
-
-     name: "ExGuard",
-     docs: [source_ref: "#{@version}", main: "Mix.Tasks.Guard", logo: "logo-white.png"],
-     source_url: "https://github.com/slashmili/ex_guard",]
+    [
+      app: :ex_guard,
+      version: @version,
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package(),
+      description: description(),
+      name: "ExGuard",
+      docs: [source_ref: "#{@version}", main: "Mix.Tasks.Guard", logo: "logo-white.png"],
+      source_url: "https://github.com/slashmili/ex_guard"
+    ]
   end
 
   def application do
@@ -23,22 +24,21 @@ defmodule ExGuard.Mixfile do
   end
 
   defp deps do
-    [{:fs, "~> 0.9"},
-     {:ex_doc, "~> 0.15.0", only: :dev},
-     {:earmark, "~> 1.2.0", only: :dev}]
+    [{:fs, "~> 0.9"}, {:ex_doc, "~> 0.15.0", only: :dev}, {:earmark, "~> 1.2.0", only: :dev}]
   end
 
   defp package do
-    [maintainers: ["Milad Rastian"],
-     licenses: ["MIT"],
-     links: %{github: "https://github.com/slashmili/ex_guard"},
-     files: ~w(lib priv LICENSE mix.exs logo-white.png) ++
-            ~w(mix.exs README.md)]
+    [
+      maintainers: ["Milad Rastian"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/slashmili/ex_guard"},
+      files: ~w(lib priv LICENSE mix.exs logo-white.png) ++ ~w(mix.exs README.md)
+    ]
   end
 
   defp description do
-     """
-     ExGuard automates various tasks by running custom rules whenever file or directories are modified.
-     """
+    """
+    ExGuard automates various tasks by running custom rules whenever file or directories are modified.
+    """
   end
 end
